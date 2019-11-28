79cbbad97b433037066559b2fb09f83843e1d98a
Kevin Mu <mukeran@mukeran.com>
2019-11-28 15:12:04 +0800

    Tiny (#1)
    * :ambulance: Directory search and dictionary
    
    * :ambulance: Normalized Renaming

--- lib/utils/DirectorySearcher.py
+++ lib/utils/DirectorySearcher.py
@@ -1,10 +1,17 @@
 # coding:utf-8
 
+from urllib.parse import urljoin
+
+import requests
+from requests.exceptions import ConnectTimeout
+from requests.exceptions import InvalidSchema
+
 
 class DirectorySearcher:
     def __init__(self, results, **kwargs):
-        self.results = results
         self.args = kwargs
+        self.results = results
+        self.directories = []
 
     @staticmethod
     def meta():
@@ -14,4 +21,22 @@ class DirectorySearcher:
         }
 
     def exec(self):
-        pass
+        with open('../../dictionary/common_directory.txt') as f:
+            lines = f.readlines()
+            count = 0
+
+            for line in lines:
+                url = urljoin(self.args['url'], line)
+                try:
+                    r = requests.get(url, timeout=5)
+                except ConnectTimeout:
+                    continue
+                except InvalidSchema:
+                    continue
+                if r.status_code in [200, 201, 301, 306, 401, 403]:
+                    self.directories.append({
+                        'path': line,
+                        'status_code': r.status_code
+                    })
+        self.results['directories'] = self.directories
+
