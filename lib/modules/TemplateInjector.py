5dd5511cf5c52b25a04159cf65ff9c417d50c4a3
Tinywangxx <m15631869019@163.com>
2019-12-17 18:03:30 +0800

    :dizzy: Updated template injector

--- lib/modules/TemplateInjector.py
+++ lib/modules/TemplateInjector.py
@@ -1,6 +1,6 @@
 # coding:utf-8
 
-from urllib.parse import urljoin
+from urllib import parse
 
 import requests
 import random
@@ -21,39 +21,77 @@ class TemplateInjector:
         }
 
     def exec(self):
-        raw_url = self.results['url']
-        url = set()
+        def set_payload():
+            randint1 = random.randint(32768, 65536)
+            randint2 = random.randint(16384, 32768)
 
-        for _url in raw_url:
-            _url.split('?', 1)
-            url.add(_url[0])
+            _sum = randint1 + randint2
 
-        randint1 = random.randint(32768, 65536)
-        randint2 = random.randint(16384, 32768)
-
-        randsum = randint1 + randint2
-
-        payload = '{{' + str(randint1) + '+' + str(randint2) + '}}'
-        check_str = str(randsum)
+            _payload = '{{' + str(randint1) + '+' + str(randint2) + '}}'
+            check_str = str(_sum)
+            return {'payload': _payload, 'check_str': check_str}
 
         header = {
             'User_Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.'
                           '2924.87 Safari/537.36'
         }
-        for test_url in url:
-            attack_dir = urljoin(test_url, payload)
-            req = requests.get(attack_dir, header=header)
-            if req.text.find(check_str) != -1:
-                self.vulnerable.append({
-                    'url': test_url,
-                    'payload': payload
-                })
 
-        self.reports.append({
-            'title': 'Server Side Template Injection Points',
-            'overview': 'Found {} SSTI point(s)'.format(len(self.results)),
-            'header': ['Path', 'Payload'],
-            'entries': list(map(lambda x: [x['url'], x['payload']], self.vulnerable))
-        })
+        raw_url = self.results['url']
+
+        for url in raw_url:
+            check = set()
+            attack_url = []
+
+            if url[0:4] != 'http':
+                url = 'http://' + url
+            parse_result = parse.urlparse(url)
+            query = parse.parse_qs(parse_result.query)
+
+            split_dir = parse_result.path.split('/')
+            _url = parse_result.scheme + '://' + parse_result.netloc
+
+            for i in range(1, len(split_dir)):
+                payload = set_payload()
+                former = '/'.join(split_dir[0:i])
+                latter = '/'.join(split_dir[i+1:])
+                check_url = _url + former + latter
+                if check_url in check:
+                    continue
+                check.add(check_url)
+                _url += former + '/' + payload['payload']
+                if latter != '':
+                    _url += '/' + latter
+                attack_url.append({'url': _url, 'payload': payload})
+
+            _url += parse_result.path
+
+            for value in query.values():
+                for i in value:
+                    q = ''
+                    payload = set_payload()
+                    for _key, _value in query.items():
+                        for j in _value:
+                            q += '&' + _key + '='
+                            if i == j:
+                                q += payload['payload']
+                            else:
+                                q += j
+                    q = '?' + q[1:]
+                    attack_url.append({'url': _url + q, 'payload': payload})
+
+            for test_url in attack_url:
+                req = requests.get(test_url, header=header)
+                if req.text.find(test_url['payload']['check_str']) != -1:
+                    self.vulnerable.append({
+                        'url': test_url['url'],
+                        'payload': test_url['payload']['payload']
+                    })
+
+            self.reports.append({
+                'title': 'Server Side Template Injection Points',
+                'overview': 'Found {} SSTI point(s)'.format(len(self.results)),
+                'header': ['Path', 'Payload'],
+                'entries': list(map(lambda x: [x['url'], x['payload']], self.vulnerable))
+            })
 
         pass
